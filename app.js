86c53b1bb63b9316f4bd9d781aaa8c31f8242682
suanna <philomath14@gmail.com>
2022-09-29 23:20:51 -0500

    basic security implemented

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -14,6 +18,9 @@ const LocalStrategy = require('passport-local');
 const Book = require('./models/book');
 
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require("helmet");
+
 
 const usersRoutes = require('./routes/users');
 const booksRoutes = require('./routes/books');
@@ -36,14 +43,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 
 const sessionConfig = {
+    name: 'cookieThing',
     secret: 'tempsecret',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -51,6 +61,62 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+/*app.use(
+    helmet({
+      contentSecurityPolicy: false,
+    })
+  );
+*/
+/*app.use(helmet({ crossOriginEmbedderPolicy: false }));*/
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/"
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://events.mapbox.com",
+    "https://res.cloudinary.com/dv5vm4sqh/"
+    //"https://i.gr-assets.com/"
+];
+const fontSrcUrls = [];
+
+
+app.use(
+    helmet.contentSecurityPolicy({
+        directives : {
+            defaultSrc : [],
+            connectSrc : [ "'self'", ...connectSrcUrls ],
+            scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+            styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+            workerSrc  : [ "'self'", "blob:" ],
+            objectSrc  : [],
+            imgSrc     : [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://images.unsplash.com/",
+                "https://i.gr-assets.com/"
+        
+
+            ],
+            fontSrc    : [ "'self'", ...fontSrcUrls ],
+            mediaSrc   : [ "https://res.cloudinary.com/dv5vm4sqh/" ],
+            childSrc   : [ "blob:" ]
+        }
+    })
+);
+
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -60,7 +126,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req,res,next) => {
-    
+    //console.log(req.query);
     //console.log(req.session);
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
